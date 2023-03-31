90117477a5a25a399cf3472e81b652879e85a6c7
Anastasiya Karabenak <karabenak.aa.@gmail.com>
2023-03-31 09:20:50 +0300

    Add Task010

---
+++ Task010/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая 
+// 1. принимает на вход трёхзначное число и 
+// 2. на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = GetSecondDigit(number);
+Console.WriteLine($"Вторая цифра числа -> {result}");
+
+int GetSecondDigit(int number)
+{
+   return number / 10 % 10;
+}
