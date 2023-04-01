ba2d1b50eea3b9e367591fcbecbdb5a42a77a16e
Anastasiya Karabenak <karabenak.aa.@gmail.com>
2023-04-01 18:45:05 +0300

    Add Task013

---
+++ Task013/Program.cs
@@ -0,0 +1,28 @@
+﻿// // Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+
+int GetThirdDigit(int number)
+{
+   int result = -1;
+   if (number > 99)
+   {
+    while (number < 1000)
+    {
+         number = number / 10;
+    }
+         result = number % 10; 
+   }
+    return result;
+}
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = GetThirdDigit(number);
+if (GetThirdDigit(number) == -1)
+Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine($"Третья цифра числа -> {result}");
+
+
+
