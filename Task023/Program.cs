1b6be15c18517d87d27fa885ab49de9509e351fe
Anastasiya Karabenak <karabenak.aa.@gmail.com>
2023-04-01 23:16:22 +0300

    Add Task023

---
+++ Task023/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+TableCube(num);
+
+void TableCube(int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        int cube = count * count * count;
+        Console.WriteLine($"{count, 3} -> {cube, 6}");
+        count++;
+    }
+}
