d8919dbe42489b3ad12e7e6f400dbf4fa73d684d
Anastasiya Karabenak <karabenak.aa.@gmail.com>
2023-04-02 19:02:47 +0300

    Add Task029

---
+++ Task029/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+if(arraySize <= 0)
+{
+Console.WriteLine($"Ошибка: введено неверное значение {arraySize}");
+return;
+}
+int[] arr = CreateArray(arraySize);
+PrintArray(arr);
+
+int[] CreateArray(int arrayLength)
+{
+int[] newArray = new int[arrayLength];
+Random random = new Random();
+for(int i = 0; i < arrayLength; i++)
+{
+newArray[i] = random.Next(0, 101);
+}
+return newArray;
+}
+
+void PrintArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i] }");
+}
+}
