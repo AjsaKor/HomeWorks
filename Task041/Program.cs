26e9cc9cff7570e660084db9df4ba85a030e5406
Anastasiya Karabenak <karabenak.aa.@gmail.com>
2023-04-05 20:05:45 +0300

    Add Task041

---
+++ Task041/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите количество чисел");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray;
+myArray = InputArray(size);
+Console.WriteLine("Ваш массив: ");
+PrintArray(myArray);
+Console.WriteLine();
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(myArray)}");
+
+
+int[] InputArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} -й элемент");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
